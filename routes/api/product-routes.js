9eb5a66210ec710f9ab2ab76c270f391421d9dbc
Korey Cheshire <korey.cheshire@gmail.com>
2021-11-02 19:13:30 -0700

    added code to models

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({include: [Category, ProductTag, Tag]})  //is this the right way to do it?
+  .then((data) => {
+    res.json(data);
+  }) .catch(err => {
+    console.log(err)
+    res.json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({include: [Category, ProductTag, Tag], where: {id:req.params.id}}) //is this the right way to do it?
+  .then((data) => {
+    res.json(data);
+  }) .catch(err => {
+    console.log(err)
+    res.json(err);
+  })
 });
 
 // create new product
@@ -91,6 +105,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({where: {id:req.params.id}})
+  .then((data) => {
+    res.json(data);
+  }) .catch(err => {
+    console.log(err)
+    res.json(err);
+  })
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,25 +4,58 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({include: [Product, ProductTag]})  //is this the right way to do it?
+  .then((data) => {
+    res.json(data);
+  }) .catch(err => {
+    console.log(err)
+    res.json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({include: [Product, ProductTag], where: {id:req.params.id}}) //is this the right way to do it?
+  .then((data) => {
+    res.json(data);
+  }) .catch(err => {
+    console.log(err)
+    res.json(err);
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((data) => {
+    res.json(data);
+  }) .catch(err => {
+    console.log(err)
+    res.json(err);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Category.update(req.body, {where: {id:req.params.id}})
+  .then((data) => {
+    res.json(data);
+  }) .catch(err => {
+    console.log(err)
+    res.json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({where: {id:req.params.id}})
+  .then((data) => {
+    res.json(data);
+  }) .catch(err => {
+    console.log(err)
+    res.json(err);
+  })
 });
 
 module.exports = router;
